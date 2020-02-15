3c07c4b86a38ec3fb243f0bbbc17821cc11a2124
Prakhar Rathore <pralhar2000@gmail.com>
2020-02-15 12:49:39 +0530

    Add files via upload
    saved the session of whatsapp

--- whatsapp.py
+++ whatsapp.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
+import logging
+from selenium.webdriver.chrome.options import Options
+LOG_FILENAME = 'logger.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+logging.info('This message should go to the log file')
 
-driver = webdriver.Chrome()
-driver.get("https://web.whatsapp.com/")
+options = webdriver.ChromeOptions()
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://web.whatsapp.com/')
+
+
