1c93d89f2d1f5f546d2f79c74f2be82c73f12bdd
skdbsp123 <56693194+skdbsp123@users.noreply.github.com>
2020-02-15 15:01:22 +0530

    Add files via upload

--- whatsapp.py
+++ whatsapp.py
@@ -1,4 +1,28 @@
+import os
 from selenium import webdriver
+import logging
+import time
 
-driver = webdriver.Chrome()
-driver.get("https://web.whatsapp.com/")
+
+def dp_click():
+    time.sleep(20)
+    print("test")
+    python_button = driver.find_element_by_xpath("//*[@id='side']/header/div[1]/div/img")
+    python_button.click()
+    return
+
+
+if __name__ == '__main__':
+    LOG_FILENAME = 'logger.log'
+    logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+    logging.info('This message should go to log file')
+    chromedriver = "C:\webdrivers\chromedriver.exe"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(chromedriver)
+
+    driver.get("https://web.whatsapp.com")
+
+    dp_click()
+
+
+
